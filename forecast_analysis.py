ce89424e5683952b9ef4e9790182c2d9e0b4240d
Mitchell travis <mitchelltravis@Mitchells-MacBook-Pro.local>
2025-10-02 15:34:21 -0500

    Use same DATABASE_ID as price variance skill

--- forecast_analysis.py
+++ forecast_analysis.py
@@ -13,8 +13,8 @@ warnings.filterwarnings('ignore')
 from skill_framework import skill, SkillParameter, SkillInput, SkillOutput
 from answer_rocket import AnswerRocketClient
 
-# Get DATABASE_ID from environment or use default
-DATABASE_ID = os.getenv("ANSWERROCKET_DATASET_ID", "7a30e8fc-e7aa-4829-9020-fa94572244da")
+# Use the same DATABASE_ID as price variance skill for pasta dataset
+DATABASE_ID = "1fd0bbbb-3b40-4cc3-b56f-456e50808817"
 
 
 @skill(
